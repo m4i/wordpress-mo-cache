62e3b1fa83c5b8fecf7e33b91c59541d2af75e09
Masaki Takeuchi <m.ishihara@gmail.com>
2012-06-28 06:37:17 +0900

    Support Wordpress 3.4

--- mo-cache.php
+++ mo-cache.php
@@ -13,78 +13,45 @@ Author URI: https://github.com/m4i/wordpress-mo-cache
 License: MIT
 */
 
-/**
- * Load and store MO object
- */
 class MOCache {
-	const GROUP = 'mo';
+	const VERSION = '2.0';
+	const GROUP   = 'mo';
 
-	private $hit  = array();
-	private $miss = array();
-
-	/**
-	 * Setup WordPress hook
-	 */
 	public static function setup() {
 		add_filter(
 			'override_load_textdomain', array( new self, 'load' ), 10, 3 );
 	}
 
-	/**
-	 * Load MO object from cache
-	 */
 	public function load( $override, $domain, $mofile ) {
-		if ( isset( $this->hit[$domain] ) ) return true;
-
-		if ( ! preg_match( '/\w+(?=\.mo$)/', $mofile, $matches ) ) return;
-		$key = "$domain:$matches[0]";
-
-		if ( function_exists( 'wp_cache_add_global_groups' ) ) {
-			wp_cache_add_global_groups( self::GROUP );
-		}
-
-		if ( $cache = wp_cache_get( $key, self::GROUP ) ) {
-			if ( is_array( $cache ) ) {
-				global $l10n;
-				$mo = new MO;
-				$mo->entries = $cache['entries'];
-				$mo->set_headers( $cache['headers'] );
-				$l10n[$domain] = $mo;
-			}
-
-			$this->hit[$domain] = true;
-			return true;
-
-		} else {
-			add_action( 'shutdown', array( $this, 'store' ) );
-
-			$this->miss[$domain] = $key;
-			return false;
-		}
-	}
-
-	/**
-	 * Store MO object in cache
-	 */
-	public function store() {
 		global $l10n;
 
+		do_action( 'load_textdomain', $domain, $mofile );
+		$mofile = apply_filters( 'load_textdomain_mofile', $mofile, $domain );
+		if ( !is_readable( $mofile ) ) return false;
+
 		if ( function_exists( 'wp_cache_add_global_groups' ) ) {
 			wp_cache_add_global_groups( self::GROUP );
 		}
+		$key = preg_replace('/[^-\w]/', '_',
+			self::VERSION . "-${GLOBALS['wp_version']}-$mofile");
 
-		foreach ( $this->miss as $domain => $key ) {
-			if ( isset( $l10n[$domain] ) ) {
-				$mo = $l10n[$domain];
-				$cache = array(
-					'entries' => $mo->entries,
-					'headers' => $mo->headers,
-				);
-			} else {
-				$cache = 'negative cache';
-			}
+		$mo = new MO();
+		if ( $cache = wp_cache_get( $key, self::GROUP ) ) {
+			$mo->entries = $cache['entries'];
+			$mo->set_headers( $cache['headers'] );
+		} else {
+			if ( !$mo->import_from_file( $mofile ) ) return false;
+			$cache = array(
+				'entries' => $mo->entries,
+				'headers' => $mo->headers,
+			);
 			wp_cache_set( $key, $cache, self::GROUP );
 		}
+
+		if ( isset( $l10n[$domain] ) )
+			$mo->merge_with( $l10n[$domain] );
+		$l10n[$domain] = &$mo;
+		return true;
 	}
 }
 
